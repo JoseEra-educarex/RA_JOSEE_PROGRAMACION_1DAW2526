66c37bb8cc00809afa9cd345fb45b21a8f5759e9
jereyesa01 <jereyesa01@educarex.es>
2025-11-13 11:13:06 +0100

    se complica

---
+++ src/unidad3/ejercicios/Ejerc_14.java
@@ -0,0 +1,92 @@
+package unidad3.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejerc_14 {
+
+	public static void main(String[] args) {
+		/*. En una veterinaria de nuestra localidad ofrece distintos tipos de análisis 
+		 * a perros. Los precios de los análisis son como siguen: 
+		 * Tipo de análisis 1= 25.00 €; Tipo de análisis 2= 36.00 €; Tipo
+			de análisis 3= 50.00 €. Realizar un programa que muestre el precio
+			 al indicar el tipo de análisis.
+				Por otra parte, existen clientes afiliados y no afiliados. 
+				En caso de que el animal pertenezca a un cliente afiliado se le 
+				descuenta un 8% al precio del análisis. Introduce en una segunda
+				versión del programa este cálculo.*/
+		
+		Scanner entrada= new Scanner(System.in);
+		
+		int opcion;
+		double precio =0;
+		
+		
+		do {
+			
+		System.out.println("------BIENVENIDO------");
+		System.out.println("Introduzca una opcion");
+		System.out.println("1. Analisis Tipo 1");
+		System.out.println("2. Analisis Tipo 2");
+		System.out.println("3. Analisis Tipo 3");
+		System.out.println("0. Salir del Programa");
+		
+		opcion=entrada.nextInt();
+			
+		switch(opcion) {
+		
+		case 1: 
+			System.out.println("ha seleccionado Analisis tipo 1");
+			System.out.println("Su precio es 25.00 Euros");
+			precio=25;
+			break;
+		case 2: 
+			System.out.println("ha seleccionado Analisis tipo 2");
+			System.out.println("Su precio es 36.00 Euros");
+			precio=36;
+			break;
+		case 3: 
+			System.out.println("ha seleccionado Analisis tipo 3");
+			System.out.println("Su precio es 50.00 Euros");
+			precio=50;
+			break;
+		case 0: 
+			System.out.println("Gracias por venir a la clinica");
+			break;
+		
+		default: System.out.println("Seleccione una opcion valida");
+		System.out.println();
+		}
+		
+		}while(opcion!=0 && opcion !=1 && opcion !=2 && opcion !=3);
+		
+		if(opcion==0) {
+		
+			System.out.println("¡Hasta Pronto!");	
+			
+		}else {
+			
+		System.out.println("¿Está afiliado a nuestra clinica?");
+		System.out.println("Los afiliados obtienen un 8% de descuento!!!");
+		String limpiarBf=entrada.nextLine();
+		String respuesta=entrada.nextLine();
+		
+	
+
+           if (respuesta.equalsIgnoreCase("si")) {
+               double nuevoPrecio = calcularNuevoPrecio(precio);
+			System.out.println("Su nuevo precio es: "+ nuevoPrecio+ " Euros");
+		}else {
+			System.out.println("¿Le gustaria afiliarse?");
+		}
+		
+		}
+		
+		entrada.close();
+
+	}
+
+	private static double calcularNuevoPrecio(double precio) {
+        return precio * 0.92; // Aplica 8% de descuento
+    }
+
+}
---
+++ src/unidad3/ejercicios/Ejerc_15.java
@@ -0,0 +1,25 @@
+package unidad3.ejercicios;
+
+public class Ejerc_15 {
+
+	public static void main(String[] args) {
+		/*Suponga que la población de bovino de la provincia Cáceres es de 354000 
+		 * y que la población aumenta 1.01 por ciento anualmente. Suponga que la 
+		 * población de ovino de Badajoz es de 410000 y que la población se reduce 0,15
+		 * por ciento anual. Escribir una aplicación que muestra las poblaciones de las
+		 * dos provincias cada año hasta que la población de Cáceres exceda a la de
+		 * los Badajoz, y muestre el número de años que se necesitó para superar.
+			Guarde el archivo como PoblacionBovina.java.*/
+
+		long N_bovino=354000;
+		long N_ovino=410000;
+		
+		double Tasa_crecimiento_bovino=1.01/100;//sumar
+		double Tasa_decrecimiento_ovino=0.15/100;//restar
+		
+	
+		
+		
+	}
+
+}
